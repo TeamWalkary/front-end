7a65d5a3b8e2d3390ef6501852de31112696e278
mandarin-sep <vheh0901@gmail.com>
2023-10-21 22:52:09 +0900

    chore: router setting

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,22 @@
-import styled from "styled-components";
+import { Routes, Route } from "react-router-dom";
+import Landing from "./pages/Landing";
+import Login from "./pages/Login";
+import SignUp from "./pages/SignUp";
+import Main from "./pages/Main";
+import CollectDiary from "./pages/CollectDiary";
+import CollectPin from "./pages/CollectPin";
+import NotFoundPage from "./pages/NotFoundPage";
 
 export default function App() {
   return (
-    <MobileContainer>
-      <h1>라우터 설정 필요!</h1>
-    </MobileContainer>
+    <Routes>
+      <Route path="/" element={<Landing />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/signUp" element={<SignUp />} />
+      <Route path="/Main" element={<Main />} />
+      <Route path="/collectDiary" element={<CollectDiary />} />
+      <Route path="/collectPin" element={<CollectPin />} />
+      <Route path="/*" element={<NotFoundPage />} />
+    </Routes>
   );
 }
-
-const MobileContainer = styled.div`
-  position: relative;
-
-  margin: 0 auto;
-`;
