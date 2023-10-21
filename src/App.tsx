d5e0f8f6fdc8fb0a1ecdd0a928b527a5b07dc0a7
mandarin-sep <vheh0901@gmail.com>
2023-10-22 00:48:03 +0900

    [feat] 글로벌 스타일 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { Routes, Route } from "react-router-dom";
+import GlobalStyle from "./GlobalStyle";
 import Landing from "./pages/Landing";
 import Login from "./pages/Login";
 import SignUp from "./pages/SignUp";
@@ -9,14 +10,17 @@ import NotFoundPage from "./pages/NotFoundPage";
 
 export default function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Landing />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/signUp" element={<SignUp />} />
-      <Route path="/Main" element={<Main />} />
-      <Route path="/collectDiary" element={<CollectDiary />} />
-      <Route path="/collectPin" element={<CollectPin />} />
-      <Route path="/*" element={<NotFoundPage />} />
-    </Routes>
+    <>
+      <GlobalStyle />
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signUp" element={<SignUp />} />
+        <Route path="/main" element={<Main />} />
+        <Route path="/collectDiary" element={<CollectDiary />} />
+        <Route path="/collectPin" element={<CollectPin />} />
+        <Route path="/*" element={<NotFoundPage />} />
+      </Routes>
+    </>
   );
 }
