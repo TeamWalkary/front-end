ac0cd5d84d8592b9453dc3c57e66e5185c5dc23d
mandarin-sep <vheh0901@gmail.com>
2023-10-23 01:05:43 +0900

    [feat] 일기작성 페이지 퍼블리싱

--- src/components/Main/MainView.tsx
+++ src/components/Main/MainView.tsx
@@ -69,10 +69,13 @@ const MainHeader = styled.header`
   padding: 0.5rem 1.6rem;
   display: flex;
   justify-content: space-between;
+  align-items: center;
   font-weight: 700;
   line-height: 2.4rem;
   font-size: 1.4rem;
   letter-spacing: -0.021rem;
+  height: 4rem;
+  box-shadow: 0px 4px 4px 0px rgba(51, 51, 51, 0.08);
 `;
 
 const MapArea = styled.div`
