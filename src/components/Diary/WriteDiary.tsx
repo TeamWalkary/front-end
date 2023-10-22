ec9a241d04fd92de72e125d4ed78d964070ced38
NYeonK <skdus728@gmail.com>
2023-10-22 21:47:41 +0900

    [ feat ] 일기 작성 기능 구현

--- src/components/Diary/WriteDiary.tsx
+++ src/components/Diary/WriteDiary.tsx
@@ -1,3 +1,53 @@
+import styled from "styled-components";
+import { useState, ChangeEvent } from "react";
+
 export default function WriteDiary() {
-  return <div>WriteDiary</div>;
+  const [title, setTitle] = useState("");
+  const [text, setText] = useState("");
+  const [imageUrl, setImageUrl] = useState<string | null>(null);
+
+  const handleChangeTitle = (e: ChangeEvent<HTMLInputElement>) => {
+    setTitle(e.target.value);
+  };
+
+  const handleChangeContent = (e: ChangeEvent<HTMLTextAreaElement>) => {
+    setText(e.target.value);
+  };
+
+  const handleChangeImage = (e: ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault();
+
+    const reader = new FileReader();
+    const file = e.target.files![0];
+
+    reader.onloadend = () => {
+      setImageUrl(reader.result as string);
+    };
+
+    if (file) {
+      reader.readAsDataURL(file);
+    }
+  };
+
+  const handleSubmit = () => {
+    // post api
+  };
+
+  return (
+    <Container>
+      <input placeholder="제목" value={title} onChange={handleChangeTitle} />
+      {imageUrl && <img src={imageUrl} alt="preview" />}
+      <textarea value={text} onChange={handleChangeContent}></textarea>
+      <div>
+        <input type="file" onChange={handleChangeImage} />
+      </div>
+      <div>{text.length} / 500</div>
+      <button onClick={handleSubmit}>작성 완료</button>
+    </Container>
+  );
 }
+
+const Container = styled.section`
+  display: flex;
+  flex-direction: column;
+`;
