c1ab2010f438e4be274918221903e291e6a61134
sojung <gonaeng20@gmail.com>
2023-10-22 18:11:37 +0900

    Feat/#3 현재위치로 이동기능

--- src/components/Main/MapView.tsx
+++ src/components/Main/MapView.tsx
@@ -1,9 +1,8 @@
 import styled from "styled-components";
-import React, { useEffect } from 'react';
-import { ReactComponent as Pin } from "../../assests/pin.svg";
-import { ReactComponent as Gps } from "../../assests/gps.svg";
+import React, { useEffect, useRef } from 'react';
+import { ReactComponent as PinIcon } from "../../assests/pin.svg";
+import { ReactComponent as GpsIcon } from "../../assests/gps.svg";
 import CurrentLoc from "../../assests/currentLocation.svg"
-
 
 declare global {
   interface Window {
@@ -11,8 +10,9 @@ declare global {
   }
 }
 
-
 const MapView: React.FC = () => {
+  const mapRef = useRef<any>(null);
+
   useEffect(() => {
     const container = document.getElementById('map')
     const options = {
@@ -21,34 +21,31 @@ const MapView: React.FC = () => {
     };
 
     // 지도를 표시할 div와 지도 옵션으로 지도를 생성합니다
-    const map = new window.kakao.maps.Map(container!, options);
+    mapRef.current = new window.kakao.maps.Map(container!, options);
 
     if (navigator.geolocation) {
 
       navigator.geolocation.getCurrentPosition(function(position) {
 
         const lat = position.coords.latitude,
-            lon = position.coords.longitude;
-
+              lon = position.coords.longitude;
         const locPosition = new window.kakao.maps.LatLng(lat, lon);
 
-        displayMarker(map, locPosition);
+        displayMarker(mapRef.current, locPosition);
         
       });
       
     } else {
 
       const locPosition = new window.kakao.maps.LatLng(37.5132612, 127.1001336);
-         
-
-      displayMarker(map, locPosition);
+      displayMarker(mapRef.current, locPosition);
       
     }
 
   }, []);
 
   
-  function displayMarker(map:any ,locPosition:any ) {
+function displayMarker(map:any ,locPosition:any ) {
 
   const imageSize = new window.kakao.maps.Size(15, 15); 
 	const markerImage = new window.kakao.maps.MarkerImage(CurrentLoc, imageSize);
@@ -64,13 +61,25 @@ const MapView: React.FC = () => {
 	map.setCenter(locPosition);
 }
 
+function handleGpsClick() {
+	if (navigator.geolocation) {
+	navigator.geolocation.getCurrentPosition(function(position) {
+			const latlng=new window.kakao.maps.LatLng(position.coords.latitude,position.coords.longitude)
+			mapRef.current.setCenter(latlng)
+			displayMarker(mapRef.current,latlng)
+	});
+} else { 
+	alert("Geolocation is not supported by this browser.");
+}
+}
+
   return (
     <Map>
       {/* 지도를 표시할 div 입니다 */}
       <div id="map" style={{ width: '100%', height: '232px'}}>
       </div>
       <MapBtns>
-        <Gps/>
+        <Gps onClick={handleGpsClick}/>
         <Pin/>
       </MapBtns>
     </Map>
@@ -91,3 +100,10 @@ const MapBtns = styled.div`
   bottom:18px;
   z-index:1;
 `;
+
+const Gps = styled(GpsIcon)`
+  cursor:pointer;
+`
+const Pin = styled(PinIcon)`
+  cursor:pointer;
+`
