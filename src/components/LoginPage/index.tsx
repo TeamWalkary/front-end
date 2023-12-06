246fcf3940e753f0bce2200ea1793920649875af
이여진 <jinspace3710@gmail.com>
2023-12-06 17:25:22 +0900

    [ fix ] svg 파일명 수정

--- src/components/LoginPage/index.tsx
+++ src/components/LoginPage/index.tsx
@@ -1,24 +1,24 @@
-import { S } from "./style";
+import { S } from './style';
 //
-import { ReactComponent as Logo } from "../../assests/newLogo.svg";
-import { ReactComponent as CloseBtn } from "../../assests/CloseBtn.svg";
-import { useNavigate } from "react-router-dom";
-import SubmitForms from "./SubmitForms";
+import { ReactComponent as Logo } from '../../assests/newLogo.svg';
+import { ReactComponent as CloseBtn } from '../../assests/closeBtn.svg';
+import { useNavigate } from 'react-router-dom';
+import SubmitForms from './SubmitForms';
 
 export default function LoginPage() {
   const navigate = useNavigate();
 
   const signUpHandler = () => {
-    navigate("/signUp");
+    navigate('/signUp');
   };
 
   const closeHandler = () => {
-    navigate("/");
+    navigate('/');
   };
 
   return (
     <S.LoginArea>
-      <Logo width="183" height="48" style={{ marginBottom: "10rem" }} />
+      <Logo width='183' height='48' style={{ marginBottom: '10rem' }} />
       <SubmitForms />
       <S.SignUpBtn onClick={signUpHandler}>회원가입</S.SignUpBtn>
       <S.CloseBtnArea onClick={closeHandler}>
