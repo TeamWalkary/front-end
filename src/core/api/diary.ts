52ca523a50ef2a149f138290fa4dbf3adbc3c5ae
NYeonK <skdus728@gmail.com>
2023-12-10 21:30:59 +0900

    [ refactor ] edit diary post api

--- src/core/api/diary.ts
+++ src/core/api/diary.ts
@@ -1,3 +1,4 @@
+import { PostData } from "../../types/diary";
 import { axiosReq } from "./axios";
 import qs from "qs";
 
@@ -8,11 +9,21 @@ function fetchDiary(date?: number) {
   return axiosReq.GET(`/apis/main/diary/${dateParams}`);
 }
 
+function postDiary(postingDiary: PostData) {
+  return axiosReq.POST(`/apis/diary`, postingDiary);
+}
+
+function patchDiary(diaryId: number, patchingDiary: PostData) {
+  return axiosReq.PATCH(`/apis/diary/${diaryId}`, patchingDiary);
+}
+
 function deleteDiary(diaryId: number) {
   return axiosReq.DELETE(`/apis/main/diary/${diaryId}`);
 }
 
 export const diaryApi = {
   fetchDiary,
+  postDiary,
+  patchDiary,
   deleteDiary,
 };
