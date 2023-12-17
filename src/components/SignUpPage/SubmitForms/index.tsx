3e9bed8bafbedf3f4ce55d9e61719e42f41fc34a
hjyang369 <ghlwls6@gmail.com>
2023-12-17 17:30:07 +0900

    [fix] 전화번호, 이메일 추가 및 유효성 검사 추가

--- src/components/SignUpPage/SubmitForms/index.tsx
+++ src/components/SignUpPage/SubmitForms/index.tsx
@@ -9,15 +9,23 @@ export default function SubmitForms() {
     id: "",
     pw: "",
     checkPassword: "",
+    phoneNumberValid: "",
+    email: "",
   };
 
   const { inputValue, handleInput } = useInputValue(initInputValue);
 
-  const eng = /^[a-zA-Z]*$/;
+  const idRegExp = /^[a-z]+[a-z0-9]{0,19}$/g;
+  const pwRegExp = /^(?=.*[a-zA-Z])(?=.*[?!@#$%^*+=-])(?=.*[0-9]).{8,16}$/;
+  const phoneRegExp = /^01(?:0|1|[6-9])(?:\d{3}|\d{4})\d{4}$/;
+  const emailRegExp =
+    /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i;
 
-  const idValid: boolean = eng.test(inputValue.id);
-  const pwValid: boolean = inputValue.pw.length > 0;
+  const idValid: boolean = idRegExp.test(inputValue.id);
+  const pwValid: boolean = pwRegExp.test(inputValue.pw);
   const checkPw: boolean = inputValue.pw === inputValue.checkPassword;
+  const phoneNumberValid: boolean = phoneRegExp.test(inputValue.phoneNumber);
+  const emailValid: boolean = emailRegExp.test(inputValue.email);
   const isValid: boolean = idValid && pwValid && checkPw;
 
   const navigate = useNavigate();
@@ -46,7 +54,7 @@ export default function SubmitForms() {
         name={"id"}
         type={"text"}
         title={"아이디"}
-        validText={"아이디를 확인해주세요."}
+        validText={"20자 이내로 입력해주세요."}
         required
         handleInput={handleInput}
         isValid={idValid}
@@ -55,10 +63,12 @@ export default function SubmitForms() {
         name={"pw"}
         type={"password"}
         title={"비밀번호"}
-        validText={"비밀번호를 확인해주세요."}
+        validText={
+          "소문자, 대문자, 특수문자 중 2개 이상 포함하여 8~16자 이내로 입력해주세요."
+        }
         required
         handleInput={handleInput}
-        isValid={true}
+        isValid={pwValid}
       />
       <Input
         name={"checkPassword"}
@@ -68,6 +78,24 @@ export default function SubmitForms() {
         required
         handleInput={handleInput}
         isValid={checkPw}
+      />
+      <Input
+        name={"phoneNumber"}
+        type={"text"}
+        title={"전화번호"}
+        validText={"01000000000의 형식으로 입력해주세요."}
+        required
+        handleInput={handleInput}
+        isValid={phoneNumberValid}
+      />
+      <Input
+        name={"email"}
+        type={"text"}
+        title={"이메일"}
+        validText={"walkary1@walkary.com 형식으로 입력해주세요."}
+        required
+        handleInput={handleInput}
+        isValid={emailValid}
       />
       <Button
         placeholder={"가입 완료하기"}
