8ae1e7e90415e40fa0082dae70afc7319cb86dcb
NYeonK <skdus728@gmail.com>
2024-01-02 18:58:42 +0900

    [ refactor ] 불필요한 코드 제거

--- src/components/LandingPage/style.ts
+++ src/components/LandingPage/style.ts
@@ -5,7 +5,7 @@ export const S = {
     display: flex;
     justify-content: center;
     align-items: center;
-    height: 90vh;
+    height: 100%;
     flex-direction: column;
   `,
 
@@ -13,15 +13,5 @@ export const S = {
     margin: 1.6rem 0 4.8rem 0;
     text-align: center;
     font-size: 16px;
-  `,
-
-  LandingBtn: styled.button`
-    padding: 0.8rem 5.2rem;
-    justify-content: center;
-    align-items: center;
-    gap: 0.4rem;
-    border-radius: 99px;
-    background: #333;
-    color: white;
   `,
 };
