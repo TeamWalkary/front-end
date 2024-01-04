95d1e413f2c6f4cff2c515614b9eecd770275d13
이여진 <jinspace3710@gmail.com>
2024-01-05 04:31:26 +0900

    [ fix ] API 요청 방식 수정

--- src/components/MainPage/CreatePinModal/index.tsx
+++ src/components/MainPage/CreatePinModal/index.tsx
@@ -99,7 +99,7 @@ export default function CreatePinModal(props: modalProps) {
           };
           axiosInstance
             .get<pinResponseType>(
-              'https://api.walkary.fun/apis/main/maps-pin',
+              `${import.meta.env.VITE_APP_BASE_URL}/main/maps-pin`,
               config
             )
             .then(res => {
